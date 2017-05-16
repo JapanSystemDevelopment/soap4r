9e5b03d85be865cfe4da1f51edaa8c34cb2adde2
Yohei Igata <yohei.igata@gmail.com>
2017-05-16 15:28:49 +0900

    coverage to optional

--- test/helper.rb
+++ test/helper.rb
@@ -4,7 +4,7 @@ require 'test/unit/xml' ## RubyJedi
 
 if RUBY_VERSION.to_f >= 1.9
   require "codeclimate-test-reporter"
-  CodeClimate::TestReporter.start
+  CodeClimate::TestReporter.start if ENV['USE_COVERAGE_REPORT']
 end
 
 ENV['DEBUG_SOAP4R'] = 'true' ## Needed to force wsdl2ruby.rb and xsd2ruby.rb to use DEVELOPMENT soap4r libs instead of installed soap4r libs
